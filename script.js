5021c9fc74d26246aa3d92195af40c0f35ae1995
Spencer Kimball <spencercowleykimball@gmail.com>
2026-04-02 13:22:06 -0600

    Cards looking better with paired fonts. Sizing and placement of the images is still not ideal. Still need to add a favoriates display as well as saving the favorites to the local storage.

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let dbz_container = document.querySelector("#DBZ_card_container")
 let curr_dbz_character;
 let element;
 let curr_image;
+let dbz_name_header;
+let dbz_stats;
 
 //// Set the variables equal to the api request string for each character
 let goku_api = "https://dragonball-api.com/api/characters/1";
@@ -33,29 +35,51 @@ async function get_api_data(api_calls_list) {
         curr_dbz_character = await request_api_data(api_call);
         // console.log(curr_dbz_character);
         
-        
+        dbz_name_header = document.createElement("h2");
+        dbz_stats = document.createElement("h3");
 
         // Set some basic css styling via js code
         element.style.backgroundColor = "rgb(173, 172, 172)";
         element.style.borderRadius = "12px";
-        element.style.width = "160px";
-        element.style.height = "200px";
+        element.style.width = "260px";
+        element.style.height = "400px";
         element.style.fontSize = "20px";
-        element.style.marginTop = "10px";
-        element.style.marginBottom = "10px";
+        // element.style.marginTop = "10px";
+        element.style.margin = "10px";
         element.style.display = "flex";
         element.style.justifyContent = "center";
-        element.style.padding = "20px";
-        element.style.fontFamily = "sans-serif";
+        element.style.padding = "10px";
+        element.style.fontFamily = "mulish";
+        element.style.display = "flex";
+        element.style.flexDirection = "column";
 
-        element.innerText = curr_dbz_character.name + ": \n ki = " + curr_dbz_character.ki + ", \n" + "maxKi = " + curr_dbz_character.maxKi
+        // Changing the header to have a font paired style
+        dbz_name_header.style.fontFamily = "Ovo";
+        dbz_name_header.innerText = curr_dbz_character.name + ":";
+        
+        // Append the heading to the current card
+        element.appendChild(dbz_name_header);
+        
+        // Changing the stats font to have a paired style
+        dbz_stats.style.fontFamily = "mulish";
+        
+        // Giving the stats on the DBZ Character to the DOM
+        dbz_stats.innerText =  "ki = " + curr_dbz_character.ki + ", \n" + "maxKi = " + curr_dbz_character.maxKi + "\n"
+        element.appendChild(dbz_stats);
 
         // Setting the curr image variable to an image object
         curr_image = document.createElement("img");
         curr_image.src = curr_dbz_character.image;
-        curr_image.style.height = "100px";
+        curr_image.style.height = "200px";
+        curr_image.style.width = "100px";
+        curr_image.style.hover 
+        
+        // Append the image to the current card
         element.appendChild(curr_image);
 
+
+
+        // Append the updated card to the container
         dbz_container.appendChild(element);
 
     }
@@ -71,3 +95,4 @@ async function get_api_data(api_calls_list) {
 populate_dbz_cards.addEventListener("click", function() {
     get_api_data(api_calls_list);
 });
+
